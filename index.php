e096d42ca1516241f8d54642082911200777e9fb
gmoraesalbuquerque <gmoraesalbuquerque@gmail.com>
2020-09-15 21:27:00 -0300

    iciciei o meu primeiro projeto

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new wcrepresentacoesecosmeticos\DB\sql();
+	$results = $sql->select("SELECT * FROM tb_users");
+	echo json_encode($results);
+
+});
+
+$app->run();
+
+ ?>
