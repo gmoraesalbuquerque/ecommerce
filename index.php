9da2a943cd347aa57a4b11ddb4c6b62c51caa551
gmoraesalbuquerque <gmoraesalbuquerque@gmail.com>
2020-09-18 19:18:25 -0300

    realizado criação do admin e do tpl do projeto

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \wcrepresentacoesecosmeticos\Page;
+use \wcrepresentacoesecosmeticos\PageAdmin;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new wcrepresentacoesecosmeticos\DB\sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
-
+	$page = new Page();
+	$page->setTpl("index");
+});
+$app->get('/admin', function() {
+	$page = new PageAdmin();
+	$page->setTpl("index");
 });
 
 $app->run();
---
+++ vendor/wcrepresentacoesecosmeticos/php-classes/src/page.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace wcrepresentacoesecosmeticos;
+use Rain\Tpl;
+
+class Page {
+	public $tpl;
+	public $options = [];
+	public $defaults = [
+		"data"=>[]
+	];
+	public function __construct($opts = array(), $tpl_dir = "/views/"){
+		$this->options = array_merge($this->defaults, $opts);
+		$config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false 
+				);
+		Tpl::configure( $config );
+		$this->tpl = new Tpl;
+		$this->setData($this->options["data"]);
+		$this->tpl->draw("header");
+	}
+	private function setData($data = array()){
+		foreach ($data as $key => $value) {
+			$this->tpl->assign($key, $value);
+		}
+	}
+	public function setTpl($name, $data = array(), $returnHTML = false){
+		$this->setData($data);
+		return $this->tpl->draw($name, $returnHTML);
+	}
+	public function __destruct(){
+		$this->tpl->draw("footer");
+	}
+
+}
+
+?>
